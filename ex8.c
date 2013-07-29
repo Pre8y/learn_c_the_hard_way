72d7b78d41b68f5fdffb9af2b693e032dd514a2c
U-Divum-Preeti\Divum <Divum@Divum-Preeti.(none)>
2013-07-29 15:07:51 +0530

    fixed segfault

--- ex8.c
+++ ex8.c
@@ -3,7 +3,7 @@
 int main(int argc, char *argv[])
 {
 	int areas[]  = {10, 12, 13, 14, 20};
-	char name="Zed";
+	char name[]="Zed";
 	char full_name[] = {
 	'Z', 'e', 'd',
 	' ', 'A', '.', ' ', 'S', 'h', 'a', 'w', '\0'
